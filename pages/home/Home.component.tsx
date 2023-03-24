45a8b93ad1d2e4eefa28433393d6b19ebb68ce19
lailei <lforoura@gmail.com>
2023-03-24 21:29:00 +0000

    begin home page

--- pages/home/Home.component.tsx
+++ pages/home/Home.component.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Page, Card, Heading } from "../../components";
 
-function Home () {
-    return <div>hello</div>
-}  
+function Home() {
+  return (
+    <Page>
+      <Card cardStyles='h-5/6'>
+        <div className="grid row-1">
+          <div className="site-title-subtitle grid row-1 gap-7">
+            <Heading text={' Lailei Forouraghi'} />
+            <h2 className="text-2xl text-center tracking-widest text-amber-900 font-serif">
+              Software Engineer
+            </h2>
+          </div>
+
+          <div className="menu grid col-2">
+          </div>
+        </div>
+      </Card>
+    </Page>
+  );
+
+}
 
 export default Home;
