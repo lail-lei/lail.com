c41e2dd47b5d96b3292a91c63c4d6d4a8eb5daaf
lail_lei <lforoura@gmail.com>
2023-03-24 22:14:06 -0400

    use menu component

--- pages/home/Home.component.tsx
+++ pages/home/Home.component.tsx
@@ -1,25 +1,23 @@
-import React from "react";
-import { Page, Card, Heading } from "../../components";
+import React from 'react';
+import { Page, Card, Heading, Menu } from '../../components';
+import { siteMenu } from '../../constants';
 
 function Home() {
   return (
     <Page>
       <Card cardStyles='h-5/6'>
-        <div className="grid row-1">
-          <div className="site-title-subtitle grid row-1 gap-7">
+        <div className='grid grid-row-1 gap-7 small-laptop:gap-10'>
+          <div className='site-title-subtitle grid grid-row-1 gap-4 small-laptop:gap-7'>
             <Heading text={' Lailei Forouraghi'} />
-            <h2 className="text-2xl text-center tracking-widest text-amber-900 font-serif">
+            <h2 className='text-xl small-laptop:text-2xl text-center tracking-widest text-amber-900 font-serif'>
               Software Engineer
             </h2>
           </div>
-
-          <div className="menu grid col-2">
-          </div>
+          <Menu options={siteMenu} />
         </div>
       </Card>
     </Page>
   );
-
 }
 
 export default Home;
