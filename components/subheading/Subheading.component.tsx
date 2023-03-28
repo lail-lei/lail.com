13de9c6c486386a810e899e051a4508964bdec3f
lail_lei <lforoura@gmail.com>
2023-03-27 21:59:25 -0400

    configurable subheading size

--- components/subheading/Subheading.component.tsx
+++ components/subheading/Subheading.component.tsx
@@ -3,15 +3,18 @@ import React from 'react';
 interface SubheadingProps {
   text: string;
   subheadingStyles?: string;
+  size?: string;
 }
 
 const Subheading: React.FC<SubheadingProps> = ({
   text,
-  subheadingStyles = '',
+  subheadingStyles,
+  size,
 }) => {
+  const textSize = size ? `text-[${size}]` : 'text-2xl';
   return (
     <h2
-      className={`text-2xl tracking-widest text-amber-900 font-serif ${subheadingStyles}`}
+      className={`${textSize} tracking-widest text-amber-900 font-serif ${subheadingStyles}`}
     >
       {text}
     </h2>
