45176cf834e329b087413d674c1f4c5380afbcfc
lail_lei <lforoura@gmail.com>
2023-03-29 18:36:37 -0400

    begin whimsy library

--- pages/whimsy/Whimsy.compoent.tsx
+++ pages/whimsy/Whimsy.compoent.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
+import { Page, Card } from '../../components';
+import { whimsies } from '../../constants/whimsies';
+import { Whimsy } from '../../types';
 
-function Whimsy () {
-    return <div>hello</div>
-}  
+function Whimsy() {
+  const renderWhimsies = () => {
+    return whimsies.map((whimsy: Whimsy) => <Card cardStyles='w-full h-1/3' />);
+  };
+  return (
+    <Page pageColor={'site-pink-100'}>
+      <div className='whimsy-library p-5 w-full h-full grid grid-cols-3 gap-5 place-items-center'>
+        {renderWhimsies()}
+      </div>
+    </Page>
+  );
+}
 
 export default Whimsy;
