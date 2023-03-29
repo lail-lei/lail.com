d46accafc6ac712e3d5f20157552f548094775f9
Lailei Forouraghi <lforoura@gmail.com>
2023-03-29 02:30:51 +0000

    core web vitals improvements

--- pages/home/Home.component.tsx
+++ pages/home/Home.component.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import Head from 'next/head'
 import { Page, Card, Heading, Menu } from '../../components';
 import Subheading from '../../components/subheading/Subheading.component';
 import { siteMenu } from '../../constants';
 
 function Home() {
   return (
-    <Page>
-      <Card centered={true} cardStyles='h-5/6 w-11/12  desktop:max-w-[1000px]'>
-        <div className='grid grid-row-1 gap-7 small-laptop:gap-10'>
-          <div className='site-title-subtitle grid grid-row-1 gap-4 small-laptop:gap-7'>
-            <Heading text={'Lailei Forouraghi'} />
-            <Subheading text={'Software Engineer'} />
+    <>
+      <Head>
+        <title>Lailei Forouraghi - Software Engineer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Portfolio site built with Nextjs + Tailwind. Hosted with AWS Amplify" />
+      </Head>
+      <Page>
+        <Card centered={true} cardStyles='h-5/6 w-11/12  desktop:max-w-[1000px]'>
+          <div className='grid grid-row-1 gap-7 small-laptop:gap-10'>
+            <div className='site-title-subtitle grid grid-row-1 gap-4 small-laptop:gap-7'>
+              <Heading text={'Lailei Forouraghi'} />
+              <Subheading text={'Software Engineer'} />
+            </div>
+            <Menu options={siteMenu} />
           </div>
-          <Menu options={siteMenu} />
-        </div>
-      </Card>
-    </Page>
+        </Card>
+      </Page>
+    </>
+
   );
 }
 
--- pages/resume/Resume.component.tsx
+++ pages/resume/Resume.component.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head'
 import { ResumeCard, Heading, Page } from '../../components';
 import CardAccordion from '../../components/card-accordion/CardAccordion.component';
 import { resumeCards } from '../../constants';
@@ -13,10 +14,17 @@ function Resume() {
   };
 
   return (
-    <Page pageStyles='gap-10 py-10'>
-      <Heading text={'Resume'} />
-      <CardAccordion width='11/12 desktop:max-w-[1000px]'>{renderResumeCards()}</CardAccordion>
-    </Page>
+    <>
+      <Head>
+        <title>Lailei Forouraghi - Resume</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Resume page in portfolio site built with Nextjs + Tailwind. Hosted with AWS Amplify" />
+      </Head>
+      <Page pageStyles='gap-10 py-10'>
+        <Heading text={'Resume'} />
+        <CardAccordion width='11/12 desktop:max-w-[1000px]'>{renderResumeCards()}</CardAccordion>
+      </Page>
+    </>
   );
 }
 
