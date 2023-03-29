8924000d94972975868509aa469e1f14c9b2e4c8
lail_lei <lforoura@gmail.com>
2023-03-29 18:36:17 -0400

    configurable page color

--- components/page/Page.component.tsx
+++ components/page/Page.component.tsx
@@ -3,15 +3,24 @@ import React, { ReactNode } from 'react';
 interface PageProps {
   children?: ReactNode;
   pageStyles?: string;
+  pageColor?: string;
 }
 
 /**
  * Styled component - single column, neutral background
  */
-const Page: React.FC<PageProps> = ({ children, pageStyles = '' }) => {
+const Page: React.FC<PageProps> = ({
+  children,
+  pageColor,
+  pageStyles = '',
+}) => {
+  const backgroundColorClass = pageColor
+    ? `bg-${pageColor}`
+    : `bg-site-neutral-200`;
+
   return (
     <div
-      className={`transition-all bg-site-neutral-200 min-h-screen grid-cols-1 grid place-items-center ${pageStyles}`}
+      className={`transition-all ${backgroundColorClass} min-h-screen grid-cols-1 grid place-items-center ${pageStyles}`}
     >
       {children}
     </div>
