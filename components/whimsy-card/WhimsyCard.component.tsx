45ae9c7d326347ea6c35ac60e24efcd6256d4caa
lail_lei <lforoura@gmail.com>
2023-04-03 18:32:06 -0400

    remove unused imports

--- components/whimsy-card/WhimsyCard.component.tsx
+++ components/whimsy-card/WhimsyCard.component.tsx
@@ -1,32 +1,33 @@
-import React, { MouseEventHandler } from 'react';
-import { Button, Card, IllustratedText, Subheading } from '..';
-import {
-    Whimsy as WhimsyType
-} from '../../types';
-
+import React from 'react';
+import { Button, Card, IllustratedText } from '..';
+import { Whimsy as WhimsyType } from '../../types';
 
 export interface WhimsyCardType extends WhimsyType {
-    cardStyles?: string;
+  cardStyles?: string;
 }
 
 const WhimsyCard: React.FC<WhimsyCardType> = ({
-    url,
-    title,
-    iconURL,
-    altText,
-    description,
-    disabled,
-    cardStyles,
+  url,
+  title,
+  iconURL,
+  altText,
+  description,
+  disabled,
+  cardStyles,
 }) => {
-
-
-    return (
-        <Card cardStyles={`place-items-start px-5 py-2 ${cardStyles || ''}`}>
-            <IllustratedText iconURL={iconURL} altText={altText} text={title} subheading containerStyles='w-full' />
-            <p>{description}</p>
-            <Button url={url} text={disabled ? 'Coming Soon' : 'Go'} disabled />
-        </Card>
-    );
+  return (
+    <Card cardStyles={`place-items-start px-5 py-2 ${cardStyles || ''}`}>
+      <IllustratedText
+        iconURL={iconURL}
+        altText={altText}
+        text={title}
+        subheading
+        containerStyles='w-full'
+      />
+      <p>{description}</p>
+      <Button url={url} text={disabled ? 'Coming Soon' : 'Go'} disabled />
+    </Card>
+  );
 };
 
 export default WhimsyCard;
