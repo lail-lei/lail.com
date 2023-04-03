e919f1dfe0001425be13f149cf290f41294acd8e
lail_lei <lforoura@gmail.com>
2023-04-03 18:32:42 -0400

    add alt text for back button

--- components/back-button/BackButton.component.tsx
+++ components/back-button/BackButton.component.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Button } from '../button';
 
-
-
 const BackButton: React.FC = () => {
-
-    return <Button text='Go Home' iconURL='/back-icon.png' url='/' iconSize={20} />
-
+  return (
+    <Button
+      text='home'
+      iconURL='/back-icon.png'
+      url='/'
+      altText='back arrow'
+      iconSize={20}
+    />
+  );
 };
 
 export default BackButton;
