361eebd7552c1cb975151405eff242e4249f4695
lail_lei <lforoura@gmail.com>
2023-04-03 18:32:32 -0400

    increased default whimsy card size

--- pages/whimsy/Whimsy.component.tsx
+++ pages/whimsy/Whimsy.component.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
 import { Page, WhimsyCard, Heading, BackButton } from '../../components';
 import { whimsies } from '../../constants/whimsies';
 import { Whimsy } from '../../types';
 
 function Whimsy() {
   const renderWhimsies = () => {
-    return whimsies.map((whimsy: Whimsy, index: number) => <WhimsyCard key={index} {...whimsy} cardStyles='h-full small-laptop:h-2/3' />);
+    return whimsies.map((whimsy: Whimsy, index: number) => (
+      <WhimsyCard
+        key={index}
+        {...whimsy}
+        cardStyles='h-full small-laptop:h-5/6'
+      />
+    ));
   };
   return (
     <>
       <Head>
         <title>Lailei Forouraghi - Whimsy</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Links to various whimsical side projects" />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta
+          name='description'
+          content='Links to various whimsical side projects'
+        />
       </Head>
       <Page pageStyles='gap-10 py-10'>
         <Heading text='Whimsies' />
